8e17d7490c14591d73156fb1b179483bdee39b0f
Mishismail <mishkaismail@icloud.com>
2024-06-22 11:14:44 +0200

    "First commit + code cleanup"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import { useSelector } from 'react-redux'
+
+import Settings from './components/Settings.js'
+import Question from './components/Question.js'
+import FinalScreen from './components/FinalScreen.js'
+
+import './App.css'
 
 function App() {
+  const questions = useSelector((state) => state.questions)
+  const questionIndex = useSelector((state) => state.index)
+
+  let component
+
+  if (questions.length && questionIndex + 1 <= questions.length) {
+    component = <Question />
+  } else if (!questions.length) {
+    component = <Settings />
+  } else {
+    component = <FinalScreen />
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-container">
+        <div className="quiz-container">{component}</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
